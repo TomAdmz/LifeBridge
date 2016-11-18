4df5308ca124e6cc532184685e26e49ca2b25610
Tom <t.adamcewicz@gmail.com>
2016-11-17 22:44:06 -0500

    Added skeleton of LifeBridge App
    All code is subject to change, just a skeleton for us to work
    with/start from.

---
+++ LifeBridge/app.js
@@ -0,0 +1,43 @@
+var express = require('express');
+
+var app = express();
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+var session = require('express-session');
+var request = require('request');
+var bodyParser = require('body-parser');
+
+var mysql = require('mysql');
+var pool = mysql.createPool({
+  host  : 'localhost',
+  user  : 'student',
+  password: 'default',
+  database: 'student'
+});
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(session({secret:'SuperSecretPassword'}));
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+app.set('port', 50000);
+
+app.get('/', function(req, res) {
+  res.render('home');
+}); 
+
+app.use(function(req,res){
+  res.status(404);
+  res.render('404');
+});
+
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  //res.type('plain/text');
+  res.status(500);
+  res.render('500');
+});
+
+app.listen(app.get('port'), function(){
+  console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
+});
