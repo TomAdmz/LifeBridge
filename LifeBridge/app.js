1f3f0bbdae564579a200e6371ea30e319bcb0af4
Jeff <lmpl1993@gmail.com>
2016-11-22 17:24:50 -0500

    Add files via upload
    Added app.get(/editform) and app.get(/edit) to this.
    
    editform will load and submit. It is sending the appropriate data as confirmed by the console.log statement I've placed in /edit. But the update in /edit is not working and I can't seem to figure it out.

--- LifeBridge/app.js
+++ LifeBridge/app.js
@@ -96,7 +96,6 @@ passport.use('local-signup', new LocalStrategy({
       if (err)
         return done(err);
        if (rows.length) {
-                req.session.error = 'That username is already in use, please try a different one.'; //inform user could not log them in
                 return done(null, false, req.flash('signupMessage', 'That e-mail is already taken.'));
         } else {
 
@@ -184,7 +183,7 @@ app.get('/', function(req, res) {
 
 //displays our signup page
 app.get('/signin', function(req, res){
-  res.render('signin', { msg: req.flash('signupMessage') });
+  res.render('signin', { message: req.flash('signupMessage') });
 });
 //sends the request through our local signup strategy, and if successful takes user to homepage, otherwise returns then to signin page
 app.post('/local-reg', passport.authenticate('local-signup', {
@@ -196,8 +195,7 @@ app.post('/local-reg', passport.authenticate('local-signup', {
 //sends the request through our local login/signin strategy, and if successful takes user to homepage, otherwise returns then to signin page
 app.post('/login', passport.authenticate('local-signin', {
   successRedirect: '/',
-  failureRedirect: '/signin',
-  failureFlash : true
+  failureRedirect: '/signin'
   }),
   function(req, res) {
             console.log("hello");
@@ -214,29 +212,33 @@ app.post('/login', passport.authenticate('local-signin', {
 //logs user out of site, deleting them from the session, and returns to homepage
 app.get('/logout', function(req, res){
   var name = req.user.fname;
-  console.log("LOGGIN OUT " + req.user.userName)
+  console.log("LOGGING OUT " + req.user.userName)
   req.logout();
   res.redirect('/');
   req.session.notice = "You have successfully been logged out " + name + "!";
 });
 
-//inbox page
-app.get('/messages', function(req, res) {
-  var userID = req.user.userID;
-  var context = {};
-  connection.query('SELECT userName, messagetxt FROM messages INNER JOIN users ON userID = sendID WHERE recID = ?', [userID], function(err, rows, fields){
-    if(err){
-      next(err);
-      return;
-    }
-    console.log(rows);
-   // res.render('home'); 
-  res.render('messages', {user: req.user}, {rows: rows});
+//edit data form
+app.get('/editForm', function(req, res){
+	res.render('edit',{user: req.user}); //render editform page with prepopulated table data
   });
-  //res.render('messages', {user: req.user}, {rows: rows});
-}); 
 
-
+  app.get('/edit', function(req, res, next) {
+	  console.log(req.query.fname);
+	  console.log(req.query.lname);
+	  connection.query("UPDATE `users` SET fname=?, lname=?, pword=? WHERE userID =?", [req.query.fname, req.query.lname, req.query.pword, req.query.userID], function(err, result){
+	  
+	 if(err){
+		  next(err);
+		  return;
+	  }
+	  
+	  
+  });
+  
+  res.render('home', {user: req.user})
+  });
+  
 app.use(function(req,res){
   res.status(404);
   res.render('404');
