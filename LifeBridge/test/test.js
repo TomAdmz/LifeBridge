db926457cb327963b4ffbca8046df953f751b302
ruizry <ruizry@oregonstate.edu>
2016-11-30 22:44:29 -0800

    For unit testing
    Basic test that first logs in using passport. To run open 2 terminals in root, node app.js to run app first and npm test in second terminal

---
+++ LifeBridge/test/test.js
@@ -0,0 +1,33 @@
+var request = require('supertest');
+var server = request.agent('http://localhost:50000');
+
+describe('GET /findmatch', function(){
+    it('login', loginUser());
+    it('uri that requires user to be logged in', function(done){
+    server
+        .get('/findmatch')                       
+        .expect(200)
+        .end(function(err, res){
+            if (err) return done(err);
+            console.log(res.body);
+            done()
+        });
+    });
+});
+
+
+function loginUser() {
+    return function(done) {
+        server
+            .post('/login')
+            .send({ email: 'tdawg', password: 't' })
+            .expect(302)
+            .expect('Location', '/')
+            .end(onResponse);
+
+        function onResponse(err, res) {
+           if (err) return done(err);
+           return done();
+        }
+    };
+};
